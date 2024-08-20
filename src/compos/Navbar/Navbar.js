a897ea7d774f3986f14898f2b3541f6487c1b00a
aayush17develOPer <timewasterme937@gmail.com>
2024-08-20 23:05:48 +0530

    Final

--- src/compos/Navbar/Navbar.js
+++ src/compos/Navbar/Navbar.js
@@ -24,7 +24,7 @@ const Navbar = () => {
             <Dropdown/>
         </li> */}
         <li>
-          <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Watchlist</a>
+          {/* <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Watchlist</a> */}
         </li>
         <li>
           <a href="/balance" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Balance & Transactions</a>
